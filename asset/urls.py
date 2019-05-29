0b085a288bbe49d1f9023f4030d254c997145f88
sxr <sxr@weiyian.com>
2019-05-29 10:57:24 +0800

    upd

--- asset/urls.py
+++ asset/urls.py
@@ -4,9 +4,9 @@ from asset import views
 
 urlpatterns = (
     url(r'^$', views.AssetListView.as_view(), name='asset-list'),
-    url(r'^/create$', views.AssetCreateView.as_view(), name='asset-create'),
-    url(r'^/update/(?P<id>\d+)/$', views.AssetUpdateView.as_view(), name='asset-update'),
-    url(r'^/delete/(?P<id>\d+)/$', views.asset_delete, name='asset-delete'),
-    url(r'^/tailf$', views.tailf, name='tailf-index'),
-    url(r'^/ws/asset$', views.tailf_websocket, name='tailf-websocket'),
+    url(r'^create/$', views.AssetCreateView.as_view(), name='asset-create'),
+    url(r'^update/(?P<id>\d+)/$', views.AssetUpdateView.as_view(), name='asset-update'),
+    url(r'^delete/(?P<id>\d+)/$', views.asset_delete, name='asset-delete'),
+    url(r'^tailf/$', views.tailf, name='tailf-index'),
+    url(r'^ws/asset/$', views.tailf_websocket, name='tailf-websocket'),
 )
