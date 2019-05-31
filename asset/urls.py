293d38dcc3aa162ae82a380f3cfc645d5b6d9c3b
sxr <sxr@weiyian.com>
2019-05-31 18:20:06 +0800

    udp

--- asset/urls.py
+++ asset/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls import url
 from asset import views
 
 urlpatterns = (
-    url(r'^$', views.asset, name='asset-list'),
+    # url(r'^$', views.asset, name='asset-list'),
+    url(r'^$', views.local_tailf, name='local-tailf'),
     url(r'^create/$', views.AssetCreateView.as_view(), name='asset-create'),
     url(r'^update/(?P<id>\d+)/$', views.AssetUpdateView.as_view(), name='asset-update'),
     url(r'^delete/(?P<id>\d+)/$', views.asset_delete, name='asset-delete'),
     url(r'^tailf/$', views.tailf, name='tailf-index'),
-    url(r'^local-tailf/$', views.local_tailf, name='local-tailf'),
+    # url(r'^local-tailf/$', views.local_tailf, name='local-tailf'),
     url(r'^local-file/$', views.local_file_list, name='local-file'),
     url(r'^ws/asset/tailf/$', views.tailf_socket, name='tailf-websocket'),
     url(r'^ws/local-asset/tailf/$', views.local_tailf_socket, name='local-tailf-websocket'),
