52535c7945d44e07be5b1f4040183480cd50a369
sxr <sxr@weiyian.com>
2019-07-01 11:15:44 +0800

    udp

--- docker.sh
+++ docker.sh
@@ -1,4 +1,8 @@
- docker run --privileged --name=test1 --restart=always -dv \
+#!/usr/bin/env bash
+docker-machine ssh default "sudo mkdir /sys/fs/cgroup/systemd"
+docker-machine ssh default "sudo mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd"
+
+docker run --privileged --name=test1 --restart=always -dv \
     /d:/d \
     -p 2000:22 -p 8000:8000 -p 80:80 -p 8001:8001 \
     registry.cn-hangzhou.aliyuncs.com/lch_docker_k/aaaaaaaa:1111
