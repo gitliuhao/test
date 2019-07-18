e860cb64992f4ec899824b2681301f962f06bf82
sxr <sxr@weiyian.com>
2019-07-18 19:03:36 +0800

    upd 文件监控

--- demoapps/asset/views/apis.py
+++ demoapps/asset/views/apis.py
@@ -14,6 +14,23 @@ class SystemFileNamePathListApi(View):
         asset_id = request.GET.get('asset_id', 0) or 0
         asset = get_object_or_404(Asset, pk=asset_id)
         xssh = ControlSsh(username=asset.username, host=asset.host, key_filename=asset.ssh_key_url())
+        _, out, _ = xssh.exec_command("ls -Rlt --time=ctime /etc")
+        d = out.read().decode()
+        data_list = d.split("\n\n")
+        text = []
+        for x in data_list:
+            folder_path, _, *file_list = x.split("\n")
+            folder_path = folder_path[:-1]
+            p_and_t = []
+            for file in file_list:
+                if file and file[0] == "-":
+                    xlist = file.split(" ")
+                    print(xlist)
+                    time, file_name= " ".join(xlist[-4:-1]), xlist[-1]
+                    p_and_t.append({'time': time, "file_path": folder_path+ "/" + file_name})
+            if p_and_t:
+                text.append(p_and_t)
+
         job_building_list = json.dumps(server.get_job_building_list())
 
         return HttpResponse(job_building_list, content_type="application/json")
