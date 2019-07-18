3f035d7033b7328a22d16c25a4f2388452ff9ea2
sxr <sxr@weiyian.com>
2019-07-18 18:33:09 +0800

    upd 文件监控

--- demoapps/asset/views/apis.py
+++ demoapps/asset/views/apis.py
@@ -4,12 +4,16 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.views.generic.base import View
 
+from asset.models import Asset
+from asset.ssh_client import ControlSsh
+
 
 class SystemFileNamePathListApi(View):
     ''' 查看远程系统路径下文件名路径 '''
     def get(self, request, *args, **kwargs):
-        jk = get_object_or_404(JenkinsConfig, pk=request.GET.get('jk_id', 0))
-        server = JenkinsServer(**jk.config_to_dict())
+        asset_id = request.GET.get('asset_id', 0) or 0
+        asset = get_object_or_404(Asset, pk=asset_id)
+        xssh = ControlSsh(username=asset.username, host=asset.host, key_filename=asset.ssh_key_url())
         job_building_list = json.dumps(server.get_job_building_list())
 
         return HttpResponse(job_building_list, content_type="application/json")
